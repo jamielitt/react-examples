93ef89400fd5dadcad2788a17fa08c656193fc52
jamielitt <jamie.littledv@gmail.com>
2025-02-07 15:56:21 +0000

    Renamed UI and added Two Way Binding example (#1)

--- src/modules/Panel1.jsx
+++ src/modules/Panel1.jsx
@@ -1,7 +1,22 @@
+import React, { useState } from 'react'
+
 function Panel1()
 {
+    const [input, setInput] = useState('')
+
+    function setInputHandler(event)
+    {
+        setInput(event.target.value);
+    }
+
     return (
-        <div>Panel 1</div>
+        <>
+        <div>This is a two-way binding example</div>
+        <div>
+            <input type="text" onChange={setInputHandler}></input>
+            <p> You have entered {input}</p>
+        </div>
+        </>
     )
 }
 
